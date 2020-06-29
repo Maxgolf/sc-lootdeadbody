76572d870ccd88e5baddff018e07da73d660e8e5
Maxgolf <52411193+Maxgolf@users.noreply.github.com>
2020-06-29 20:47:51 +0300

    changed author discord

--- __resource.lua
+++ __resource.lua
@@ -2,7 +2,7 @@ resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
 
 name 'sc-lootdeadbody'
 description 'Loot dead bodies with esx_inventoryhud support'
-author 'Ledasgm ( Max Golf#8946 )'
+author '( Max Golf#0001 )'
 
 client_scripts {
     'cl-lootdeadbody.lua'
