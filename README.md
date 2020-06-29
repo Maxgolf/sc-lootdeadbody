2e94c68e0ee7262e328363eb36ea6675df920bdd
Maxgolf <52411193+Maxgolf@users.noreply.github.com>
2020-06-29 20:44:27 +0300

    fixed weapons not showing

--- README.md
+++ README.md
@@ -4,6 +4,7 @@ Loot dead bodies for ESX framework
 
 # Requirements
 - [ESX FRAMEWORK](https://github.com/ESX-Org/es_extended)
+- [esx_policejob](https://github.com/ESX-Org/esx_policejob)
 
 # Join discord if you need help!
 [Discord](https://discord.gg/Zyq7kjw)
--- cl-lootdeadbody.lua
+++ cl-lootdeadbody.lua
@@ -19,16 +19,82 @@ RegisterCommand("loot", function(source, args, raw)
 	local closestPlayer, closestDistance = ESX.Game.GetClosestPlayer()
 	if IsPlayerDead(closestPlayer) then 
 		if closestPlayer ~= -1 and closestDistance <= 3.0 then
+
+			-- YOU CAN ADD YOUR PROGRESS BAR HERE
+			
 			OpenBodySearchMenu(closestPlayer)
 		end
 	end
 end, false)
 
 
-
-
-
-
 function OpenBodySearchMenu(player)
-    TriggerEvent("esx_inventoryhud:openPlayerInventory", GetPlayerServerId(player), GetPlayerName(player))
+
+	ESX.TriggerServerCallback('esx_policejob:getOtherPlayerData', function(data)
+
+		local elements = {}
+
+		for i=1, #data.accounts, 1 do
+
+			if data.accounts[i].name == 'black_money' and data.accounts[i].money > 0 then
+
+				table.insert(elements, {
+					label    = _U('confiscate_dirty', ESX.Math.Round(data.accounts[i].money)),
+					value    = 'black_money',
+					itemType = 'item_account',
+					amount   = data.accounts[i].money
+				})
+
+				break
+			end
+
+		end
+
+		table.insert(elements, {label = _U('guns_label'), value = nil})
+
+		for i=1, #data.weapons, 1 do
+			table.insert(elements, {
+				label    = _U('confiscate_weapon', ESX.GetWeaponLabel(data.weapons[i].name), data.weapons[i].ammo),
+				value    = data.weapons[i].name,
+				itemType = 'item_weapon',
+				amount   = data.weapons[i].ammo
+			})
+		end
+
+		table.insert(elements, {label = _U('inventory_label'), value = nil})
+
+		for i=1, #data.inventory, 1 do
+			if data.inventory[i].count > 0 then
+				table.insert(elements, {
+					label    = _U('confiscate_inv', data.inventory[i].count, data.inventory[i].label),
+					value    = data.inventory[i].name,
+					itemType = 'item_standard',
+					amount   = data.inventory[i].count
+				})
+			end
+		end
+
+		ESX.UI.Menu.Open('default', GetCurrentResourceName(), 'body_search',
+		{
+			title    = _U('search'),
+			align    = 'center',
+			elements = elements,
+		},
+		function(data, menu)
+
+			local itemType = data.current.itemType
+			local itemName = data.current.value
+			local amount   = data.current.amount
+
+			if data.current.value ~= nil then
+				TriggerServerEvent('esx_policejob:confiscatePlayerItem', GetPlayerServerId(player), itemType, itemName, amount)
+				OpenBodySearchMenu(player)
+			end
+
+		end, function(data, menu)
+			menu.close()
+		end)
+
+	end, GetPlayerServerId(player))
+
 end
